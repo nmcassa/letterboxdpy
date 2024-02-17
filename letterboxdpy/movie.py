004676ca7d84b52b99b89ebaa17f2683145fc26a
FastFingertips <bugra.coskun@outlook.com>
2024-02-17 20:52:09 +0300

    refactor: replace repeated scraping logic in `Movie` class with `Scraper` class
    - apply `Scraper` class
    - addassert decorator for funcs
    - edit syntax for readability

--- letterboxdpy/movie.py
+++ letterboxdpy/movie.py
@@ -1,67 +1,47 @@
-import json
-import requests
-from bs4 import BeautifulSoup
-from json import JSONEncoder
+from scraper import Scraper
+
+from json import (
+  JSONEncoder,
+  dumps as json_dumps,
+  loads as json_loads,
+)
 
 
 class Movie:
     DOMAIN = 'https://letterboxd.com'
-    FILM_URL = DOMAIN + '/film/'
 
     def __init__(self, slug: str) -> None:
-        self.url = self.FILM_URL + slug
-        page = self.get_parsed_page(self.url)
+        assert isinstance(slug, str), f"Movie slug must be a string, not {type(slug)}"
 
-        script = page.find("script", type="application/ld+json")
-        script = json.loads(script.text.split('*/')[1].split('/*')[0]) if script else None
+        self.url = f'{self.DOMAIN}/film/{slug}'
+        self.scraper = Scraper(self.DOMAIN)
+
+        dom = self.scraper.get_parsed_page(self.url)
+
+        script = dom.find("script", type="application/ld+json")
+        script = json_loads(script.text.split('*/')[1].split('/*')[0]) if script else None
 
         # one line contents
-        self.movie_tmdb_link(page)
+        self.movie_tmdb_link(dom)
         self.movie_poster(script)
-        self.movie_rating(page, script)
-        self.movie_year(page, script)
+        self.movie_rating(dom, script)
+        self.movie_year(dom, script)
         # long contents
-        self.movie_description(page)
-        self.movie_director(page)
-        self.movie_genre(page)
-        self.movie_popular_reviews(page)
+        self.movie_description(dom)
+        self.movie_director(dom)
+        self.movie_genre(dom)
+        self.movie_popular_reviews(dom)
 
     def __str__(self):
         return self.jsonify()
 
     def jsonify(self) -> str:
-        return json.dumps(self, indent=4,cls=Encoder)
-
-    def get_parsed_page(self, url: str) -> None:
-        # This fixes a blocked by cloudflare error i've encountered
-        headers = {
-            "referer": self.DOMAIN,
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-        }
-        try:
-            response = requests.get(url, headers=headers, timeout=30)
-        except requests.exceptions.Timeout:
-            raise Exception("Request timeout, site may be down")
-
-        dom = BeautifulSoup(response.text, "lxml")
-
-        if response.status_code != 200:
-            message = dom.find("section", {"class": "message"})
-            message = message.strong.text if message else None
-            messages = json.dumps({
-                'code': response.status_code,
-                'reason': str(response.reason),
-                'url': url,
-                'message': message
-            }, indent=2)
-            raise Exception(messages)
-
-        return dom
+        return json_dumps(self, indent=2, cls=Encoder)
 
     # letterboxd.com/film/?
-    def movie_director(self, page: BeautifulSoup) -> list:
+    def movie_director(self, dom) -> list:
         self.directors = []
-        data = page.find("div",{"id": ["tab-crew"], })
+        data = dom.find("div",{"id": ["tab-crew"], })
         if type(data) != type(None):
             data = data.find_all("a")
             for item in data:
@@ -69,8 +49,8 @@ class Movie:
                     self.directors.append(item.text)
 
     # letterboxd.com/film/?
-    def movie_rating(self, page: BeautifulSoup, script: dict=None) -> float:
-        elem = page.find('span', attrs={'class': 'average-rating'})
+    def movie_rating(self, dom, script: dict=None) -> float:
+        elem = dom.find('span', attrs={'class': 'average-rating'})
         rating = float(elem.text) if elem else None
         rating = rating if rating else (
              script['aggregateRating']['ratingValue'] if script else None
@@ -78,8 +58,8 @@ class Movie:
         self.rating = float(rating)
 
     # letterboxd.com/film/?
-    def movie_year(self, page: BeautifulSoup, script: dict=None) -> int:
-        elem = page.find('small', attrs={'class': 'number'})
+    def movie_year(self, dom, script: dict=None) -> int:
+        elem = dom.find('small', attrs={'class': 'number'})
         year = int(elem.text) if elem else None
         year = year if year else (
              script['releasedEvent']['startDate'] if script else None
@@ -87,10 +67,10 @@ class Movie:
         self.year = int(year)
 
     # letterboxd.com/film/?
-    def movie_genre(self, page: BeautifulSoup) -> list:
+    def movie_genre(self, dom) -> list:
         genres = []
 
-        data = page.find("div",{"id": ["tab-genres"], })
+        data = dom.find("div",{"id": ["tab-genres"], })
         if data is not None:
             data = data.find_all("a")
             for item in data:
@@ -109,41 +89,34 @@ class Movie:
             self.poster = None
 
     # letterboxd.com/film/?
-    def movie_description(self, page: BeautifulSoup) -> str:
-        elem = page.find("meta", attrs={'name':'description'})
+    def movie_description(self, dom) -> str:
+        elem = dom.find("meta", attrs={'name':'description'})
         # elem_section = page.find("div", attrs={'class': 'truncate'}).text
         self.description = elem['content'] if elem else None
 
     # letterboxd.com/film/?
-    def movie_popular_reviews(self, page: BeautifulSoup) -> dict:
-        data = page.find("ul", {"class": ["film-popular-review"], })
-        data = data.find_all("div", {"class": ["film-detail-content"], })
+    def movie_popular_reviews(self, dom) -> dict:
+        data = dom.find("ul", {"class": ["film-popular-review"], })
+        items = data.find_all("div", {"class": ["film-detail-content"], })
 
         self.popular_reviews = []
-        for item in data:
+        for item in items:
             curr = {}
 
-            try:
-                curr['reviewer'] = item.find("strong", {"class": ["name"], }).text
-            except:
-                curr['reviewer'] = None
+            owner = item.find("strong", {"class": ["name"], })
+            rating = item.find("span", {"class": ['rating'], })
+            review = item.find("div", {"class": ['body-text'], }).p
 
-            try:
-                curr['rating'] = item.find("span", {"class": ['rating'], }).text
-            except:
-                curr['rating'] = None
-
-            try:
-                curr['review'] = item.find("div", {"class": ['body-text'], }).findChild("p").text
-            except:
-                curr['review'] = None
+            curr['reviewer'] = owner.text if owner else None
+            curr['rating'] = rating.text if rating else None
+            curr['review'] = review.text if review else None
 
             self.popular_reviews.append(curr)
 
     # letterboxd.com/film/?
-    def movie_tmdb_link(self, page: BeautifulSoup) -> str:
+    def movie_tmdb_link(self, dom) -> str:
         try:
-            div = page.find("p", {"class": ["text-link text-footer"], })
+            div = dom.find("p", {"class": ["text-link text-footer"], })
             a = div.find_all("a")
             for item in a:
                 if item['href'].find('themoviedb.org/') != -1:
@@ -151,70 +124,70 @@ class Movie:
         except: 
             self.tmdb_link = None
 
-# letterboxd.com/film/?/details
-def movie_details(movie: Movie) -> dict:
-    if type(movie) != Movie:
-        raise Exception("Improper parameter")
-
-    page = movie.get_parsed_page("/".join([movie.url, "details"]))
-
-    res = {}
-    studio = []
-    country = []
-    language = []
-
-    div = page.find("div", {"id": ["tab-details"], })
-    a = div.find_all("a")
-
-    for item in a:
-        if item['href'][1:7] == 'studio':
-            studio.append(item.text)
-        elif item['href'][7:14] == 'country':
-            country.append(item.text)
-        elif item['href'][7:15] == 'language':
-            language.append(item.text)
-
-    res['Country'] = country
-    res['Studio'] = studio
-    res['Language'] = language
-
-    return res
-
-# letterboxd.com/film/?/members
-def movie_watchers(movie: Movie) -> dict:
-    if type(movie) != Movie:
-        raise Exception("Improper parameter")
-
-    page = movie.get_parsed_page("/".join([movie.url, "members"]))
-
-    res = {}
-    res['watch_count'] = 0
-    res['fan_count'] = 0
-    res['like_count'] = 0
-    res['review_count'] = 0
-    res['list_count'] = 0
-
-    div = page.find("div", {"id": ["content-nav"], })
-    if type(div) != type(None):
-        a = div.find_all("a")
-
-        for item in a:
-            if item.get('title'):
-                if item['title'].find('people',-6) != -1:
-                    res['watch_count'] = item['title'][:-7].replace(',','')
-                elif item['title'].find('fans',-4) != -1:
-                    res['fan_count'] = item['title'][:-5].replace(',','')
-                elif item['title'].find('likes',-5) != -1:
-                    res['like_count'] = item['title'][:-6].replace(',','')
-                elif item['title'].find('reviews',-7) != -1:
-                    res['review_count'] = item['title'][:-8].replace(',','')
-                elif item['title'].find('lists',-5) != -1:
-                    res['list_count'] = item['title'][:-6].replace(',','')
-    return res
-
 class Encoder(JSONEncoder):
     def default(self, o):
         return o.__dict__
+
+# -- DECORATORS --
+
+def assert_movie_instance(func):
+    def wrapper(movie):
+        assert isinstance(movie, Movie), f"movie parameter must be a {Movie.__name__} instance"
+        return func(movie)
+    return wrapper
+
+# -- FUNCTIONS --
+
+# letterboxd.com/film/?/details
+@assert_movie_instance
+def movie_details(movie: Movie) -> dict:
+    dom = movie.scraper.get_parsed_page("/".join([movie.url, "details"]))
+    dom = dom.find("div", {"id": ["tab-details"]})
+
+    data = {
+        'country': [],
+        'studio': [],
+        'language': []
+    }
+
+    for a in dom.find_all("a"):
+        if a['href'][1:7] == 'studio':
+            data['studio'].append(a.text)
+        elif a['href'][7:14] == 'country':
+            data['country'].append(a.text)
+        elif a['href'][7:15] == 'language':
+            data['language'].append(a.text)
+
+    return data
+
+# letterboxd.com/film/?/members
+@assert_movie_instance
+def movie_watchers(movie: Movie) -> dict:
+    dom = movie.scraper.get_parsed_page("/".join([movie.url, "members"]))
+    dom = dom.find("div", {"id": ["content-nav"]})
+
+    data = {
+        'watch_count': 0,
+        'fan_count': 0,
+        'like_count': 0,
+        'review_count': 0,
+        'list_count': 0
+    }
+
+    if dom:
+        for a in dom.find_all("a"):
+            if a.get('title'):
+                if a['title'].find('people',-6) != -1:
+                    data['watch_count'] = a['title'][:-7].replace(',','')
+                elif a['title'].find('fans',-4) != -1:
+                    data['fan_count'] = a['title'][:-5].replace(',','')
+                elif a['title'].find('likes',-5) != -1:
+                    data['like_count'] = a['title'][:-6].replace(',','')
+                elif a['title'].find('reviews',-7) != -1:
+                    data['review_count'] = a['title'][:-8].replace(',','')
+                elif a['title'].find('lists',-5) != -1:
+                    data['list_count'] = a['title'][:-6].replace(',','')
+    return data
 
 if __name__ == "__main__":
     import sys
