c80cf37534830a4730f01f885c565afb9542aaaf
FastFingertips <bugra.coskun@outlook.com>
2024-09-06 11:03:01 +0300

    feat(utils_parser): add parse_review_date function for consistent date parsing
    - added `parse_review_date` function to `utils_parser.py` for handling date formats
    - refactored `user_reviews` and `user_liked_reviews` in `user.py` to use `parse_review_date`

--- letterboxdpy/utils/utils_parser.py
+++ letterboxdpy/utils/utils_parser.py
@@ -1,7 +1,42 @@
 import re
 
-def extract_numeric_text(tag) -> int:
-  try:
-    return int(re.sub(r"[^0-9]", '', tag))
-  except Exception:
-    return None
+MONTH_ABBREVIATIONS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+def extract_numeric_text(text: str) -> int:
+    """
+    Extracts numeric characters from a string and returns them as an integer.
+    Returns None if an error occurs.
+    """
+    try:
+        numeric_value = int(re.sub(r"[^0-9]", '', text))
+        return numeric_value
+    except Exception:
+        return None
+
+def parse_review_date(review_log_type, review_date):
+    """
+    Parses review date depending on log type.
+    returns: {'year': 2024, 'month': 1, 'day': 1}
+    """
+    if review_log_type == 'Added':
+        try:
+            # ISO 8601 format: '2024-01-01T05:45:00.268Z'
+            date_parts = map(int, review_date.time['datetime'][:10].split('-'))
+            parsed_date = dict(zip(['year', 'month', 'day'], date_parts))
+        except (KeyError, ValueError) as e:
+            raise ValueError(f"Error parsing ISO date format: {e}")
+    else:
+        try:
+            # '01 Jan 2024' format
+            date_parts = review_date.text.split()
+            parsed_date = {
+                'year': int(date_parts[2]),
+                'month': MONTH_ABBREVIATIONS.index(date_parts[1]) + 1,
+                'day': int(date_parts[0])
+            }
+            
+        except (IndexError, ValueError) as e:
+            raise ValueError(f"Error parsing written date format: {e}")
+    
+    return parsed_date
