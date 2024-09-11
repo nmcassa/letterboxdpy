4ac561bfd15a260bef8bdd79304b492391bb4ec5
FastFingertips <bugra.coskun@outlook.com>
2024-09-11 05:13:08 +0300

    feat(constants): add project constants for ratings, genres, and month abbreviations

---
+++ letterboxdpy/constants/project.py
@@ -0,0 +1,11 @@
+VALID_RATINGS = {0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5}
+GENRES = [
+    "action", "adventure", "animation", "comedy", "crime",
+    "documentary", "drama", "family", "fantasy", "history",
+    "horror", "music", "mystery", "romance", "science-fiction",
+    "thriller", "tv-movie", "war", "western"
+]
+MONTH_ABBREVIATIONS = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+]
