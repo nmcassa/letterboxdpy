bf3d7597542d5b7efbeb30259847efcbb6ce1cfd
fastfingertips <46646991+fastfingertips@users.noreply.github.com>
2024-09-12 21:40:57 +0300

    Add Films Class (#55)
    - Created the Films class to handle movie scraping from Letterboxd.
    - Added a function to print movies in a formatted list.
    - Future features are planned and placed in the Future class for now.
    - Refactored examples in the main block for better clarity and demonstration.

--- letterboxdpy/utils/utils_transform.py
+++ letterboxdpy/utils/utils_transform.py
@@ -12,3 +12,12 @@ def index_to_month(month_index):
     if 1 <= month_index <= 12:
         return MONTH_ABBREVIATIONS[month_index - 1]
     return None
+
+def get_ajax_url(url: str) -> str:
+    """
+    this function returns the ajax url of the given url.
+    """
+    x = '.com/films'
+    ax = ".com/films/ajax"
+    
+    return url if ax in url else url.replace(x, ax)
