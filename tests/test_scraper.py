692179953267c508af6a0d86a7ec3cce00d63f6f
FastFingertips <bugra.coskun@outlook.com>
2024-09-21 03:33:41 +0300

    test(scraper): rename variable and refactor test functions
    - renamed `wrong_film_url` to `invalid_film_url` for better clarity.

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -9,16 +9,16 @@ class TestScraper(unittest.TestCase):
         self.scraper = Scraper("letterboxd.com")
 
         self.valid_film_url = "https://letterboxd.com/film/dune-part-two/"
-        self.wrong_film_url = "https://letterboxd.com/film/duneparttwo/"  
+        self.invalid_film_url = "https://letterboxd.com/film/duneparttwo/"
 
     def test_valid_film_url(self):
         self.assertIsInstance(
             self.scraper.get_parsed_page(self.valid_film_url), BeautifulSoup
         )
     
-    def test_wrong_film_url(self):
+    def test_invalid_film_url(self):
         try:
-            self.scraper.get_parsed_page(self.wrong_film_url)
+            self.scraper.get_parsed_page(self.invalid_film_url)
             self.fail()
         except Exception:
             self.assertTrue(True)
