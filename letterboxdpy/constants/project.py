cb5aa190844e2f0a7d2688053b92a5e151cbab75
FastFingertips <bugra.coskun@outlook.com>
2024-09-22 13:22:42 +0300

    feat(constants): add date/time constants and organize existing constants

--- letterboxdpy/constants/project.py
+++ letterboxdpy/constants/project.py
@@ -1,12 +1,20 @@
+from datetime import datetime
+
+# Date/Time Constants
+CURRENT_YEAR = datetime.now().year
+MONTH_ABBREVIATIONS = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+]
+
+# Domain/URL Constants
 DOMAIN = "https://letterboxd.com"
+
+# Movie-Related Constants
 VALID_RATINGS = {0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5}
 GENRES = [
     "action", "adventure", "animation", "comedy", "crime",
     "documentary", "drama", "family", "fantasy", "history",
     "horror", "music", "mystery", "romance", "science-fiction",
     "thriller", "tv-movie", "war", "western"
-]
-MONTH_ABBREVIATIONS = [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 ]
