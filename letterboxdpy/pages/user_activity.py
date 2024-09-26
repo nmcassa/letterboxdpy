a627e8d76b4f06e75dba507ed6fed7265c42f1e1
FastFingertips <bugra.coskun@outlook.com>
2024-09-26 15:07:10 +0300

    feat(user): add UserPages class to handle user activity
    - introduced `UserPages` class within `User`, allowing for easier extension of user-related pages like `activity`.
    - moved `user_activity` to `pages` module and integrated it into `UserPages`.
    - added `get_activity()` method to `User` class to retrieve user activity via the `UserPages` class.

---
+++ letterboxdpy/pages/user_activity.py
@@ -0,0 +1,133 @@
+from datetime import datetime
+from letterboxdpy.scraper import parse_url
+from letterboxdpy.constants.project import DOMAIN
+
+
+class UserActivity:
+
+    def __init__(self, username: str) -> None:
+        self.username = username
+        # https://letterboxd.com/<username>/activity/
+        self.activity_url = f"{DOMAIN}/ajax/activity-pagination/{self.username}"
+        
+    def get_activity(self): return extract_user_activity(self.username, self.activity_url)
+
+def extract_user_activity(username: str, ajax_url: str) -> dict:
+
+    def get_event_type(section) -> tuple:
+        """
+        Extracts the event type and log ID from the section.
+        """
+        event_type = None
+        if any(x.startswith('-') for x in section['class']):
+            event_type = list(filter(lambda x: x.startswith('-'), section['class']))[0][1:]
+        return event_type
+
+    def process_activity_log(section, event_type) -> dict:
+        """
+        Processes the activity log depending on the event type.
+        """
+        def parse_datetime(date_string: str) -> datetime:
+            """
+            Parses a datetime string, handling microseconds if present.
+            """
+            try:
+                return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+            except ValueError:
+                return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+
+        def process_review_log() -> dict:
+            """
+            Processes the review-specific log data.
+            """
+            detail = section.find("div", {"class": "film-detail-content"})
+            log_title = detail.p.text.strip()
+            log_type = log_title.split()[-1]
+            film = detail.h2.find(text=True)
+
+            rating = section.find("span", {"class": ["rating"], })
+            rating = int(rating['class'][-1].split('-')[-1]) if rating else None
+
+            film_year = detail.h2.small.text
+            film_year = int(film_year) if film_year else None
+
+            review = detail.find("div", {"class": ["body-text"], })
+            spoiler = bool(review.find("p", {"class": ["contains-spoilers"], }))
+            review = review.find_all('p')[1 if spoiler else 0:]
+            review = '\n'.join([p.text for p in review])
+
+            return {
+                'type': log_type,
+                'title': log_title,
+                'film': film,
+                'film_year': film_year,
+                'rating': rating,
+                'spoiler': spoiler,
+                'review': review
+            }
+
+        def process_basic_log() -> dict:
+            """
+            Processes the basic log data.
+            """
+            log_title = section.p.text.strip()
+            log_type = log_title.split()[1]
+            log_data = {'log_type': log_type, 'title': log_title}
+
+            if log_type == 'followed':
+                username = section.find("a", {"class": "target"})['href'].replace('/', '')
+                log_data['username'] = username
+            elif log_type == 'liked':
+                # display_name = section.find("strong",{"class":["name"]}).text.replace('\u2019s',"")
+                username = section.find("a", {"class": "target"})['href'].split('/')[1]
+                log_data['username'] = username
+            elif log_type == 'watched':
+                film = section.find("a",{"class":["target"]}).text.split('  ')[-1].strip()
+                log_data['film'] = film if film else None
+
+            return log_data
+
+        log_id = section["data-activity-id"]
+        date = parse_datetime(section.find("time")['datetime'])
+        log_data = {
+            'event_type': event_type,
+            'time': {
+                'year': date.year,
+                'month': date.month,
+                'day': date.day,
+                'hour': date.hour,
+                'minute': date.minute,
+                'second': date.second
+            }
+        }
+
+        if event_type == 'review':
+            log_data.update(process_review_log())
+        elif event_type == 'basic':
+            log_data.update(process_basic_log())
+
+        return {log_id: log_data}
+
+    data = {
+        'user': username,
+        'logs': {},
+        'total_logs': 0
+    }
+
+    dom = parse_url(ajax_url)
+    sections = dom.find_all("section")
+
+    if not sections:
+        # User {username} has no activity.
+        return data
+
+    for section in sections:
+        event_type = get_event_type(section)
+        if event_type in ('review', 'basic'):
+            log_data = process_activity_log(section, event_type)
+            data['logs'].update(log_data)
+            data['total_logs'] = len(data['logs'])
+        elif 'no-activity-message' in section['class']:
+            break
+
+    return data
--- letterboxdpy/user.py
+++ letterboxdpy/user.py
@@ -17,15 +17,24 @@ from letterboxdpy.scraper import parse_url
 from letterboxdpy.encoder import Encoder
 from letterboxdpy.avatar import Avatar
 from letterboxdpy.constants.project import DOMAIN
+from letterboxdpy.pages import (
+    user_activity
+)
 
 
 class User:
+
+    class UserPages:
+
+        def __init__(self, username: str) -> None:
+            self.activity = user_activity.UserActivity(username)
 
     def __init__(self, username: str) -> None:
         assert re.match("^[A-Za-z0-9_]*$", username), "Invalid username"
 
         self.username = username.lower()
         self.url = f"{DOMAIN}/{self.username}"
+        self.pages = self.UserPages(self.username)
 
         dom = parse_url(self.url)
 
@@ -38,6 +47,9 @@ class User:
 
     def jsonify(self) -> dict:
         return json_loads(self.__str__())
+
+    def get_activity(self) -> dict:
+        return self.pages.activity.get_activity()
 
     # letterboxd.com/?
     def user_avatar(self, dom) -> str:
@@ -603,130 +615,6 @@ def user_wrapped(user: User, year: int=2024) -> dict:
     }
 
     return wrapped
-
-# letterboxd.com/?/activity
-# letterboxd.com/ajax/activity-pagination/?/
-@assert_instance(User)
-def user_activity(user: User) -> dict:
-    BASE_URL = f"{DOMAIN}/ajax/activity-pagination/{user.username}"
-
-    def get_event_type(section) -> tuple:
-        """
-        Extracts the event type and log ID from the section.
-        """
-        event_type = None
-        if any(x.startswith('-') for x in section['class']):
-            event_type = list(filter(lambda x: x.startswith('-'), section['class']))[0][1:]
-        return event_type
-
-    def process_activity_log(section, event_type) -> dict:
-        """
-        Processes the activity log depending on the event type.
-        """
-        def parse_datetime(date_string: str) -> datetime:
-            """
-            Parses a datetime string, handling microseconds if present.
-            """
-            try:
-                return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
-            except ValueError:
-                return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-
-        def process_review_log() -> dict:
-            """
-            Processes the review-specific log data.
-            """
-            detail = section.find("div", {"class": "film-detail-content"})
-            log_title = detail.p.text.strip()
-            log_type = log_title.split()[-1]
-            film = detail.h2.find(text=True)
-
-            rating = section.find("span", {"class": ["rating"], })
-            rating = int(rating['class'][-1].split('-')[-1]) if rating else None
-
-            film_year = detail.h2.small.text
-            film_year = int(film_year) if film_year else None
-
-            review = detail.find("div", {"class": ["body-text"], })
-            spoiler = bool(review.find("p", {"class": ["contains-spoilers"], }))
-            review = review.find_all('p')[1 if spoiler else 0:]
-            review = '\n'.join([p.text for p in review])
-
-            return {
-                'type': log_type,
-                'title': log_title,
-                'film': film,
-                'film_year': film_year,
-                'rating': rating,
-                'spoiler': spoiler,
-                'review': review
-            }
-
-        def process_basic_log() -> dict:
-            """
-            Processes the basic log data.
-            """
-            log_title = section.p.text.strip()
-            log_type = log_title.split()[1]
-            log_data = {'log_type': log_type, 'title': log_title}
-
-            if log_type == 'followed':
-                username = section.find("a", {"class": "target"})['href'].replace('/', '')
-                log_data['username'] = username
-            elif log_type == 'liked':
-                # display_name = section.find("strong",{"class":["name"]}).text.replace('\u2019s',"")
-                username = section.find("a", {"class": "target"})['href'].split('/')[1]
-                log_data['username'] = username
-            elif log_type == 'watched':
-                film = section.find("a",{"class":["target"]}).text.split('  ')[-1].strip()
-                log_data['film'] = film if film else None
-
-            return log_data
-
-        log_id = section["data-activity-id"]
-        date = parse_datetime(section.find("time")['datetime'])
-        log_data = {
-            'event_type': event_type,
-            'time': {
-                'year': date.year,
-                'month': date.month,
-                'day': date.day,
-                'hour': date.hour,
-                'minute': date.minute,
-                'second': date.second
-            }
-        }
-
-        if event_type == 'review':
-            log_data.update(process_review_log())
-        elif event_type == 'basic':
-            log_data.update(process_basic_log())
-
-        return {log_id: log_data}
-
-    data = {
-        'user': user.username,
-        'logs': {},
-        'total_logs': 0
-    }
-
-    dom = parse_url(BASE_URL)
-    sections = dom.find_all("section")
-
-    if not sections:
-        # User {user.username} has no activity.
-        return data
-
-    for section in sections:
-        event_type = get_event_type(section)
-        if event_type in ('review', 'basic'):
-            log_data = process_activity_log(section, event_type)
-            data['logs'].update(log_data)
-            data['total_logs'] = len(data['logs'])
-        elif 'no-activity-message' in section['class']:
-            break
-
-    return data
 
 # https://letterboxd.com/?/lists/
 @assert_instance(User)
