aebe5b474c2cfec973470d72e7edc2d94c507d8b
FastFingertips <bugra.coskun@outlook.com>
2024-09-26 15:38:00 +0300

    feat(user.py): add user_lists support to User class
    - imported user_lists module and instantiated UserLists in the User class.
    - added get_lists method to retrieve user lists.

---
+++ letterboxdpy/pages/user_lists.py
@@ -0,0 +1,99 @@
+from letterboxdpy.utils.utils_parser import extract_and_convert_shorthand
+from letterboxdpy.scraper import parse_url
+from letterboxdpy.constants.project import DOMAIN
+
+
+class UserLists:
+
+    def __init__(self, username: str) -> None:
+        self.username = username
+        self.url = f"{DOMAIN}/{self.username}/lists"
+        
+    def get_lists(self) -> dict: return extract_user_lists(self.username)
+
+def extract_user_lists(username: str) -> dict:
+    BASE_URL = f"{DOMAIN}/{username}/lists/"
+    LISTS_PER_PAGE = 12
+    SELECTORS = {
+        'list_set': ('section', {'class': 'list-set'}),
+        'lists': ('section', {'class': 'list'}),
+        'title': ('h2', {'class': 'title'}),
+        'description': ('div', {'class': 'body-text'}),
+        'value': ('small', {'class': 'value'}),
+        'likes': ('a', {'class': 'icon-like'}),
+        'comments': ('a', {'class': 'icon-comment'}),
+    }
+
+    def fetch_page_data(page: int) -> dict:
+        """Fetch and parse page data."""
+        dom = parse_url(f'{BASE_URL}/page/{page}')
+        list_set = dom.find(*SELECTORS['list_set'])
+        return list_set
+
+    def extract_list_data(item) -> dict:
+        """Extract data from a list item."""
+
+        def get_id() -> str:
+            return item['data-film-list-id']
+
+        def get_title() -> str:
+            return item.find(*SELECTORS['title']).text.strip()
+
+        def get_description() -> str:
+            description = item.find(*SELECTORS['description'])
+            if description:
+                paragraphs = description.find_all('p')
+                return '\n'.join([p.text for p in paragraphs])
+            return description
+
+        def get_url() -> str:
+            return DOMAIN + item.find(*SELECTORS['title']).a['href']
+
+        def get_slug() -> str:
+            return get_url().split('/')[-2]
+
+        def get_count() -> int:
+            return int(item.find(*SELECTORS['value']).text.split()[0].replace(',', ''))
+
+        def get_likes() -> int:
+            likes = item.find(*SELECTORS['likes'])
+            likes = extract_and_convert_shorthand(likes)
+            return likes
+
+        def get_comments() -> int:
+            comments = item.find(*SELECTORS['comments'])
+            comments = int(comments.text.split()[0].replace(',','')) if comments else 0
+            return comments
+
+        return {
+             get_id(): {
+                'title': get_title(),
+                'slug': get_slug(),
+                'description': get_description(),
+                'url': get_url(),
+                'count': get_count(),
+                'likes': get_likes(),
+                'comments': get_comments()
+                }
+            }
+
+    data = {'lists': {}}
+    page = 1
+    while True:
+        list_set = fetch_page_data(page)
+        if not list_set:
+            break
+
+        lists = list_set.find_all(*SELECTORS['lists'])
+        for item in lists:
+            list_data = extract_list_data(item)
+            data['lists'] |= list_data
+
+        if len(lists) < LISTS_PER_PAGE:
+            break
+        page += 1
+
+    data['count'] = len(data['lists'])
+    data['last_page'] = page
+
+    return data
--- letterboxdpy/user.py
+++ letterboxdpy/user.py
@@ -19,7 +19,8 @@ from letterboxdpy.pages import (
     user_activity,
     user_diary,
     user_films,
-    user_likes
+    user_likes,
+    user_lists
 )
 
 
@@ -32,6 +33,7 @@ class User:
             self.diary = user_diary.UserDiary(username)
             self.films = user_films.UserFilms(username)
             self.likes = user_likes.UserLikes(username)
+            self.lists = user_lists.UserLists(username)
 
     def __init__(self, username: str) -> None:
         assert re.match("^[A-Za-z0-9_]*$", username), "Invalid username"
@@ -75,6 +77,9 @@ class User:
         return self.pages.likes.get_liked_films()
     def get_liked_reviews(self) -> dict:
         return self.pages.likes.get_liked_reviews()
+    
+    def get_lists(self) -> dict:
+        return self.pages.lists.get_lists()
 
     # letterboxd.com/?
     def user_avatar(self, dom) -> str:
@@ -359,95 +364,6 @@ def user_reviews(user: User) -> dict:
             data['count'] = len(data['reviews'])
             data['last_page'] = page
             break
-
-    return data
-
-# https://letterboxd.com/?/lists/
-@assert_instance(User)
-def user_lists(user: User) -> dict:
-    BASE_URL = f"{user.url}/lists/"
-    LISTS_PER_PAGE = 12
-    SELECTORS = {
-        'list_set': ('section', {'class': 'list-set'}),
-        'lists': ('section', {'class': 'list'}),
-        'title': ('h2', {'class': 'title'}),
-        'description': ('div', {'class': 'body-text'}),
-        'value': ('small', {'class': 'value'}),
-        'likes': ('a', {'class': 'icon-like'}),
-        'comments': ('a', {'class': 'icon-comment'}),
-    }
-
-    def fetch_page_data(page: int) -> dict:
-        """Fetch and parse page data."""
-        dom = parse_url(f'{BASE_URL}/page/{page}')
-        list_set = dom.find(*SELECTORS['list_set'])
-        return list_set
-
-    def extract_list_data(item) -> dict:
-        """Extract data from a list item."""
-
-        def get_id() -> str:
-            return item['data-film-list-id']
-
-        def get_title() -> str:
-            return item.find(*SELECTORS['title']).text.strip()
-
-        def get_description() -> str:
-            description = item.find(*SELECTORS['description'])
-            if description:
-                paragraphs = description.find_all('p')
-                return '\n'.join([p.text for p in paragraphs])
-            return description
-
-        def get_url() -> str:
-            return DOMAIN + item.find(*SELECTORS['title']).a['href']
-
-        def get_slug() -> str:
-            return get_url().split('/')[-2]
-
-        def get_count() -> int:
-            return int(item.find(*SELECTORS['value']).text.split()[0].replace(',', ''))
-
-        def get_likes() -> int:
-            likes = item.find(*SELECTORS['likes'])
-            likes = extract_and_convert_shorthand(likes)
-            return likes
-
-        def get_comments() -> int:
-            comments = item.find(*SELECTORS['comments'])
-            comments = int(comments.text.split()[0].replace(',','')) if comments else 0
-            return comments
-
-        return {
-             get_id(): {
-                'title': get_title(),
-                'slug': get_slug(),
-                'description': get_description(),
-                'url': get_url(),
-                'count': get_count(),
-                'likes': get_likes(),
-                'comments': get_comments()
-                }
-            }
-
-    data = {'lists': {}}
-    page = 1
-    while True:
-        list_set = fetch_page_data(page)
-        if not list_set:
-            break
-
-        lists = list_set.find_all(*SELECTORS['lists'])
-        for item in lists:
-            list_data = extract_list_data(item)
-            data['lists'] |= list_data
-
-        if len(lists) < LISTS_PER_PAGE:
-            break
-        page += 1
-
-    data['count'] = len(data['lists'])
-    data['last_page'] = page
 
     return data
 
